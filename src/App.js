8bb91aa953a0da2aed7488599068e2d58782abcf
Rahul Parihar <rahulpariharmalii@gmail.com>
2024-12-17 00:20:23 +0530

    Added TMDB error notice and added skeleton card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import AllRoutes from "./routes/AllRoutes";
-import { Header, Footer} from "../src/components/index";
+import { Header, Footer, TMDBErrorNotice } from "../src/components/index";
 
 function App() {
   return (
     <div className="App">
-      {/* <TMDBErrorNotice /> */}
+      <TMDBErrorNotice />
       <Header />
       <AllRoutes />
       <Footer />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ import { NavLink } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="bg-white shadow dark:bg-gray-800">
-      <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col items-center md:flex-row md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 text-center dark:text-gray-400">
+      <div className="mx-auto p-4 flex flex-col items-center justify-center text-center">
+        <span className="text-sm text-gray-500 dark:text-gray-400 mb-4">
           © 2024{" "}
           <a href="https://flowbite.com/" className="hover:underline">
             Movievrese
           </a>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-col items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 md:flex-row md:mt-0">
+        <ul className="flex flex-col items-center text-sm font-medium text-gray-500 dark:text-gray-400 md:flex-row md:justify-center md:mt-0">
           <li className="mb-2 md:mb-0 md:mr-6 animate-fade-in delay-100">
             <NavLink to="https://www.instagram.com/rahulpariharr_/" className="hover:underline">
               Instagram
