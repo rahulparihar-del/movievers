ce09da3f2677ddca654297b8994af7cf2b83f32a
Rahul Parihar <rahulpariharmalii@gmail.com>
2024-12-17 00:13:31 +0530

    Added TMDB error notice and added skeleton card

--- src/components/TMDBErrorNotice.js
+++ src/components/TMDBErrorNotice.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const TMDBErrorNotice = () => {
   return (
-    <div className='bg-[#1C5AAE] text-center py-2'>
-      <p className='text-white text-base sm:text-sm'>If the movie data is not visible, please enable a VPN to access the content</p>
+    <div className='bg-[#1C5AAE] text-center py-2 sm:py-3 md:py-4'>
+      <p className='text-white text-sm sm:text-base md:text-lg lg:text-xl'>
+        If the movie data is not visible, please enable a VPN to access the content
+      </p>
     </div>
   )
 }
