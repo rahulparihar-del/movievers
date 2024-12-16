314600f6233017e966a1f9e8c8106a4b424c411d
Rahul Parihar <rahulpariharmalii@gmail.com>
2024-12-17 00:24:15 +0530

    Added TMDB error notice and added skeleton card

--- src/components/Header.js
+++ src/components/Header.js
@@ -192,7 +192,7 @@ const Header = () => {
                   name="search"
                   type="text"
                   id="search-navbar"
-                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="block w-[90%] p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Search..."
                   autoComplete="off"
                 />
